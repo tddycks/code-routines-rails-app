b6beb3ec81313218fcf03167c8f08525bb41e2cc
tddycks <tadzio.smith@gmail.com>
2017-03-01 15:46:59 -0500

    added basic layout and devise roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  enum role: [:user, :admin]
   has_many :workouts
+
+  after_initialize :set_user_role
+
+  def set_user_role
+    self.role ||= :user
+  end
+
 end
