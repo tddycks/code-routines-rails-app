3287d0b624e89b6b10acbc23087888ad6c2e3c69
tddycks <tadzio.smith@gmail.com>
2017-03-01 15:12:48 -0500

    added nested workout routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
 
   def index
+    if user_signed_in?
+      @workouts = Workout.all
+    else
+      render 'static/index' 
+    end
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "home#index"
+  resources :users do
+    resources :workouts, only: [:index, :edit, :show, :new]
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
