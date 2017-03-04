10f0accea13a28493481af9581a80015444f5371
tddycks <tadzio.smith@gmail.com>
2017-03-04 15:21:23 -0500

    refactored add or display bio into application helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def display_or_add_bio(user)
+    if user.bio.present?
+      user.bio
+    elsif  user == current_user
+      link_to "Add Bio", edit_user_path(user)
+    end
+  end
 end
