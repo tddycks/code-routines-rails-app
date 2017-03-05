b83a4bf55f5b0efae82d052909a094c5d1ab1caa
tddycks <tadzio.smith@gmail.com>
2017-03-04 21:08:53 -0500

    add set_workout/set_focus methods to focus controller

--- app/controllers/focuses_controller.rb
+++ app/controllers/focuses_controller.rb
@@ -1,27 +1,33 @@
 class FocusesController < ApplicationController
+  before_action :set_focus, only: [:edit, :update, :destroy]
+  before_action :set_workout, only: [:edit, :update]
 
   def edit
-    @focus = Focus.find_by(id: params[:id])
-    @workout = Workout.find_by(id: params[:workout_id])
   end
 
   def update
-    @focus = Focus.find_by(id: params[:id])
     if @focus.update(focus_params)
       redirect_to user_workout_path(current_user, params[:workout_id])
     else
-      @workout = Workout.find_by(id: params[:workout_id])
       render :edit
     end
   end
 
   def destroy
-    Focus.find_by(id: params[:id]).destroy
+    @focus.destroy
     redirect_to user_workout_path(current_user, params[:workout_id])
   end
 
 
   private 
+
+    def set_focus
+      @focus = Focus.find_by(id: params[:id])
+    end
+
+    def set_workout
+      @workout = Workout.find_by(id: params[:workout_id])
+    end
 
     def focus_params
       params.require(:focus).permit(:name, :description, :duration)
